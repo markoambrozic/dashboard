4d84298e369608350ca872974cdb22c73d7e3eee
Lamp <dmvoicestudios@gmail.com>
2019-01-05 16:55:25 +0100

    Registered and added discovery

--- src/main/java/com/kumuluz/ee/samples/microservices/simple/CartApplication.java
+++ src/main/java/com/kumuluz/ee/samples/microservices/simple/CartApplication.java
@@ -3,8 +3,9 @@ package com.kumuluz.ee.samples.microservices.simple;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
-
+import com.kumuluz.ee.discovery.annotations.RegisterService;
 
 @ApplicationPath("/")
+@RegisterService
 public class CartApplication extends Application{
 }
