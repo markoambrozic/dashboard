cb5b40a232080db2c8daa0f79c1a1f392ce9dd2b
Lamp <dmvoicestudios@gmail.com>
2019-01-06 20:13:33 +0100

    dashboard

--- src/main/java/com/kumuluz/ee/samples/microservices/simple/DashboardResource.java
+++ src/main/java/com/kumuluz/ee/samples/microservices/simple/DashboardResource.java
@@ -1,19 +1,25 @@
 package com.kumuluz.ee.samples.microservices.simple;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kumuluz.ee.logs.LogManager;
 import com.kumuluz.ee.logs.Logger;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.List;
+
 import com.kumuluz.ee.logs.cdi.Log;
-import org.json.JSONArray;
+
+
 import org.json.JSONObject;
+
+
+
 
 
 @Path("/dashboard")
@@ -28,11 +34,14 @@ public class DashboardResource {
 
     private static final Logger LOG = LogManager.getLogger(DashboardResource.class.getName());
 
+
+
     @GET
     public Response getDashboard() {
         JSONObject obj = new JSONObject();
 
         obj.put("dashboard", "fancy info about people");
+
 
         return Response.ok(obj.toString()).build();
     }
